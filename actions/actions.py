9c4e4c0526eaae3c9b8a67ae843be30896faf91c
anchit1501 <1501.anchit@gmail.com>
2023-01-21 18:32:18 +1100

    Action Called

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,28 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionBookMeeting(Action):
+
+    def name(self) -> Text:
+        return "action_book_meeting"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print('Hiii')
+    
+        # name = tracker.get_slot("attendee")
+        # date = tracker.get_slot("date")
+        # time = tracker.get_slot("time")
+
+
+        # print("Booked with " +name+" on "+date+" at "+time)
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
